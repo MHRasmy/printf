2b3e550fb54978983fdb5e393b1f48dc77cadaa9
MHRasmy <18063@stemegypt.edu.eg>
2023-03-28 18:45:40 -0700

    enhancing readability

--- functions1.c
+++ functions1.c
@@ -75,7 +75,6 @@ int print_octal(va_list types, char buffer[],
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
-/************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
 /**
  * print_hexadecimal - Prints an unsigned number in hexadecimal notation
  * @types: Lista of arguments
